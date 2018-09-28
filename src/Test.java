3767338ea9411656d31c33abd59b02673f994ee5
Talgat <tairovtalgat@gmail.com>
2018-09-28 21:01:18 +0600

    call methods with reflection

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,35 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 
 public class Test
 {
-    public static void main(String[] args) throws Exception
+    static void test(Class c, String... args)
     {
-        practice1.Example.main(null);
+
+        try
+        {
+            Method m = c.getDeclaredMethod("main", String[].class);
+            m.invoke(null, new Object[] {args});
+        }
+        catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+
+
+    public static void main(String[] args)
+    {
+        /**
+         * practice1();
+         * practice2();
+         */
+
+        test(practice1.Example.class);
+        test(practice2.Example.class);
     }
 }
+
+
