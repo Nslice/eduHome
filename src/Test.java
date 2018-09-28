eed52fc5f55a9fb9b8e4e53c7e167d22852589a2
Talgat <tairovtalgat@gmail.com>
2018-09-28 20:55:42 +0600

    небольшой пример рефлексии, в runtime устанавливается какой вызвать метод

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,36 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+
 public class Test
 {
+    static void test(Class c, String... args)
+    {
+
+        try
+        {
+            Method m = c.getDeclaredMethod("main", String[].class);
+            m.invoke(null, new Object[] {args});
+        }
+        catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+
+
     public static void main(String[] args)
     {
-        System.out.println("Test.main");
+        /**
+         * home1(double, double);
+         * home2("info.txt");
+         * home3(
+         */
+
+        test(home1.Example.class, "13", "6.2");
+        test(home2.Example.class, "info.txt");
     }
 }
+
+
