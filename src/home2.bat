d7e9db3bde8c5644752eba8930d9a338c3850e7a
Talgat <tairovtalgat@gmail.com>
2018-10-13 20:10:54 +0600

    example batch scripting command for java

---
+++ src/home2.bat
@@ -0,0 +1,5 @@
+chpcp 1251
+mkdir classes
+javac -d classes -encoding utf8 home2\space\Planet.java home2\Example.java
+java home2.Example C:\Users\Talgat\Documents\Projects\Java\CONSOLE_TEST\Home\info.txt
+PAUSE
