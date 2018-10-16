94b9ebb6ac678965d2248e936e21da2cec414a16
Talgat <tairovtalgat@gmail.com>
2018-10-17 05:22:17 +0600

    made the definition of "RightTriangle" class in another file

--- src/home1/Example.java
+++ src/home1/Example.java
@@ -30,59 +30,6 @@ class Time
 }
 
 
-class RightTriangle
-{
-    private double cathetus1;
-    private double cathetus2;
-    private double hypotenuse;
-    //-------------------------------------------------------------------------
-
-    RightTriangle(double cathetus1, double cathetus2)
-    {
-        this.cathetus1 = cathetus1;
-        this.cathetus2 = cathetus2;
-        this.hypotenuse = Math.sqrt(cathetus1 * cathetus1 + cathetus2 * cathetus2);
-    }
-
-
-    double getCathetus_1()
-    {
-        return cathetus1;
-    }
-
-    double getCathetus_2()
-    {
-        return cathetus2;
-    }
-
-    double getHypotenuse()
-    {
-        return hypotenuse;
-    }
-
-    double perimeter()
-    {
-        return cathetus1 + cathetus2 + hypotenuse;
-    }
-
-    double area()
-    {
-        return (cathetus1 * cathetus2) / 2.0;
-    }
-
-    double[] angles()
-    {
-        double[] val = new double[3];
-        val[0] = 90;
-        if (cathetus1 > cathetus2)
-            val[1] = Math.toDegrees(Math.sin(cathetus1 / hypotenuse));
-        else
-            val[1] = Math.toDegrees(Math.cos(cathetus2 / hypotenuse));
-        val[2] = 180 - val[1] - val[0];
-        return val;
-    }
-}
-
 
 class Week
 {
@@ -95,6 +42,7 @@ class Week
         return daysName[n - 1];
     }
 }
+
 
 
 public class Example
---
+++ src/home1/RightTriangle.java
@@ -0,0 +1,54 @@
+package home1;
+
+public class RightTriangle
+{
+    private double cathetus1;
+    private double cathetus2;
+    private double hypotenuse;
+    //-------------------------------------------------------------------------
+
+    public RightTriangle(double cathetus1, double cathetus2)
+    {
+        this.cathetus1 = cathetus1;
+        this.cathetus2 = cathetus2;
+        this.hypotenuse = Math.sqrt(cathetus1 * cathetus1 + cathetus2 * cathetus2);
+    }
+
+
+    public double getCathetus_1()
+    {
+        return cathetus1;
+    }
+
+    public double getCathetus_2()
+    {
+        return cathetus2;
+    }
+
+    public double getHypotenuse()
+    {
+        return hypotenuse;
+    }
+
+    public double perimeter()
+    {
+        return cathetus1 + cathetus2 + hypotenuse;
+    }
+
+    public double area()
+    {
+        return (cathetus1 * cathetus2) / 2.0;
+    }
+
+    public double[] angles()
+    {
+        double[] val = new double[3];
+        val[0] = 90;
+        if (cathetus1 > cathetus2)
+            val[1] = Math.toDegrees(Math.sin(cathetus1 / hypotenuse));
+        else
+            val[1] = Math.toDegrees(Math.cos(cathetus2 / hypotenuse));
+        val[2] = 180 - val[1] - val[0];
+        return val;
+    }
+}
