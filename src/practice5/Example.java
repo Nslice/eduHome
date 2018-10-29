d0428e94bfe658149e7221e0f77cb094ab1ef790
Talgat <tairovtalgat@gmail.com>
2018-10-29 22:49:08 +0600

    finally prac5

--- src/practice5/Example.java
+++ src/practice5/Example.java
@@ -3,9 +3,6 @@ package practice5;
 /*
     https://regex101.com
     https://regexone.com
-    https://ru.wikipedia.org/wiki/Регулярные_выражения
-
-
 
     (clear regex):
     [ ]         - символьный класс
@@ -17,66 +14,103 @@ package practice5;
 
 import other.Show;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Example
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws java.io.IOException
     {
-
-
-//        Pattern p = Pattern.compile("\\bgr[a%e]y\\b");
-//        Matcher m = p.matcher("gray dfhgdf gr%y ]]]gr%yhfjghjghj grey");
-//
-//        while (m.find())
-//        {
-//            System.out.println(m.start() + "-" + m.end() + "     sqe: " + m.group() );
-//        }
-
-        Domashka.go();
-
-
-        System.out.println("Done.");
-    }
-}
-
-
-class Domashka
-{
-    public static void go()
-    {
+        //ex1:
+        Show.show(1);
         String str = "This is Jim and Timothy";
-        String regex = "[TJ]im";
-
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(str);
-
+        Matcher m = Pattern.compile("[TJ]im").matcher(str);
         while (m.find())
         {
             System.out.println(m.group() + " : " + m.start() + "-" + m.end());
         }
+        Show.getch();
 
 
         //ex2:
         Show.show(2);
+        str = "This is Jim and Tom";
+        m.reset(str);
+        System.out.println("before: " + str);
         str = m.replaceAll("John");
-        System.out.println("after replace: " + str);
+        System.out.println(" after: " + str);
+        Show.getch();
 
 
         //ex3:
         Show.show(3);
-        StringBuffer sb = new StringBuffer("My dad and My mom");
-        System.out.println(sb);
-        p = Pattern.compile("My");
-        m = p.matcher(sb);
+        str = "My dad and My mom";
+        System.out.println("before: " + str);
+        StringBuffer sb = new StringBuffer();
+        m = Pattern.compile("My").matcher(str);
 
         while (m.find())
         {
             m.appendReplacement(sb, "Our");
         }
-        System.out.println(sb);
+        m.appendTail(sb);
+        str = sb.toString();
+        System.out.println(" after: " + str);
+        Show.getch();
 
 
+        //ex4:
+        Show.show(4);
+        str = "The words are town torn ton toon house.";
+        m = Pattern.compile("\\st(\\w)*o(\\w)*").matcher(str);
+        while (m.find())
+        {
+            System.out.println(m.group() + "  " + m.start() + " " + m.end());
+        }
+        Show.getch();
+
+
+        //ex5:
+        Show.show(5);
+        FileReader freader = null;
+        try
+        {
+            freader = new FileReader(args[0]);
+        }
+        catch (FileNotFoundException | ArrayIndexOutOfBoundsException ex)
+        {
+            ex.printStackTrace();
+            return;
+        }
+        BufferedReader fin = new BufferedReader(freader);
+        m = Pattern.compile("^a").matcher("");
+
+        while ((str = fin.readLine()) != null)
+        {
+            m.reset(str);
+            if (m.find())
+                System.out.println(str.split(" ")[0]);
+        }
+        Show.getch();
+
+
+        //ex6:
+        Show.show(6);
+        str = "This is line 1\nHere is line 2\nThis is line 3\n";
+        m = Pattern.compile("\\d$").matcher(str);
+        while (m.find())
+        {
+            System.out.println("subsequence: " + m.group());
+            System.out.println("    indexes: " + m.start() + "-" + m.end());
+        }
+
+
+        freader.close();
+        System.out.println("Done.");
     }
 }
+
+
