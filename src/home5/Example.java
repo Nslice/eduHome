8537c98bb7a895accc42ca1f307397ea557a2fca
Talgat <tairovtalgat@gmail.com>
2018-11-11 15:38:24 +0600

    up

--- src/home5/Example.java
+++ src/home5/Example.java
@@ -3,14 +3,29 @@ package home5;
 
 
 import home5.transport.IManufacturer;
+import home5.transport.Vehicle;
+import home5.transport.sky.Airplane;
 
+
+/**
+ * НАПИСАТЬ ГДЕ downcast, а где upcast
+ */
 
 public class Example
 {
+
     public static void main(String[] args)
     {
         System.out.println("Example.main");
         IManufacturer prod;
 
+        try
+        {
+            IManufacturer p = new Airplane(2, 3 , 3, 3, 5000);
+        }
+        catch (Vehicle.CountryError ex)
+        {
+            ex.printStackTrace();
+        }
     }
 }
