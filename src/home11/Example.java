7070964000106f3282357f0b45a6a3cc86ba4d04
Talgat <tairovtalgat@gmail.com>
2018-12-17 05:16:10 +0600

    diff-prefer

--- src/home11/Example.java
+++ src/home11/Example.java
@@ -5,63 +5,7 @@ import home11.space.Planet;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
-class Container<T>
-{
-    private ArrayList<T> list;
-    //-------------------------------------------------------------------------------
 
-    public Container()
-    {
-        this.list = new ArrayList<T>();
-    }
-
-    Container(T... args)
-    {
-        this.list = new ArrayList<T>();
-        for (T obj : args)
-            this.push_back(obj);
-    }
-
-    public int size()
-    {
-        return list.size();
-    }
-
-    void clear()
-    {
-        list.clear();
-    }
-
-    void push_back(T... data)
-    {
-        for (T obj : data)
-            push_back(obj);
-    }
-
-    void push_back(T data)
-    {
-
-        list.add(list.size(), data);
-    }
-
-    T pop_back()
-    {
-        if (list.size() > 0)
-        {
-            T obj = list.get(list.size() - 1);
-            list.remove(list.size() - 1);
-            return obj;
-        }
-        else return null;
-    }
-
-    void show()
-    {
-        for (T o : list)
-            System.out.println(o);
-    }
-
-}
 
 public class Example
 {
@@ -72,7 +16,7 @@ public class Example
 
 
         System.out.println("\n\n\n");
-        Container<Planet> list = new Container<>();
+        Box<Planet> list = new Box<>();
         list.pop_back();
 
         list.push_back(new Planet("Earth"),
@@ -87,7 +31,7 @@ public class Example
         list.show();
 
 
-        Container<String> strings = new Container<>("Sun", "Oracle", "Oreo", "Grep");
+        Box<String> strings = new Box<>("Sun", "Oracle", "Oreo", "Grep");
         strings.show();
 
 
