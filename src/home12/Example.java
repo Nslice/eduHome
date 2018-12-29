d2a707c665cfd0cb8fd78a06831382a04c651f44
talgat <tairovtalgat@gmail.com>
2018-12-29 14:15:38 +0600

    up 12 ,14

--- src/home12/Example.java
+++ src/home12/Example.java
@@ -1,23 +1,27 @@
 package home12;
 
-class MyThread extends Thread
+import java.nio.charset.Charset;
+import java.util.Comparator;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+class ThreadEx1 extends Thread
 {
-    MyThread(String name)
+    ThreadEx1(String name)
     {
         super(name);
-        System.out.println(name + " STARTED");
+        System.out.println(getName() + " STARTED");
     }
 
     @Override
     public void run()
     {
-        int i = 0;
-        while (i < 3)
+        for (int i = 0; i < 3; i++)
         {
-            System.out.print("System.gc ");
-            System.out.println(i++);
             try
             {
+                System.out.println("message " + i);
                 Thread.sleep(900);
             }
             catch (InterruptedException e)
@@ -25,13 +29,14 @@ class MyThread extends Thread
                 e.printStackTrace();
             }
         }
+        System.gc();
+        System.runFinalization();
     }
 
     @Override
     protected void finalize() throws Throwable
     {
         System.out.println(getName() + " STOPPED");
-
     }
 }
 
@@ -77,25 +82,132 @@ class SubThread2 extends Thread
 }
 
 
+class ThreadEx3 extends Thread
+{
+    private static int sNumber = 0;
+    private int threadNumber;
+    private int count = 5;
+
+    public ThreadEx3()
+    {
+        this.threadNumber = ++sNumber;
+    }
+
+    public void run()
+    {
+        while (true)
+        {
+            System.out.println("Thread #" + threadNumber + " (" + count + ")");
+            count--;
+            try
+            {
+                Thread.sleep(300);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+            if (count == 0)
+            {
+                System.out.println("Thread #" + threadNumber + " stopped ");
+                return;
+            }
+        }
+    }
+
+}
+
+
+class ThreadEx4 implements Runnable
+{
+    Thread th;
+    int iter;
+
+    public ThreadEx4(String name, int iter)
+    {
+        this.iter = iter;
+        th = new Thread(this, name);
+        th.start();
+    }
+
+    @Override
+    public void run()
+    {
+        synchronized (System.out)
+        {
+            for (int i = 1; i <= iter; i++)
+            {
+                try
+                {
+                    System.out.print(th.getName() + " - " + i + "; ");
+                    Thread.sleep(new Random().nextInt(500));
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
 
 public class Example
 {
 
-    public static void main(String[] args)
+
+    public static void main(String[] args) throws Exception
     {
-        try
-        {
-            SubThread2 th2 = new SubThread2();
-            SubThread1 th1 = new SubThread1(th2);
-            th1.start();
-            th1.join();
 
-        }
-        catch (InterruptedException e)
-        {
-            e.printStackTrace();
-        }
+        System.out.println("Example.main");
+        Thread th = null;
+//        for (int i = 0; i < 5; i++)
+//        {
+//            th = new ThreadEx3();
+//            th.start();
+//        }
+//        try
+//        {
+//            th.join();
+//        }
+//        catch (InterruptedException e)
+//        {
+//            e.printStackTrace();
+//        }
 
-        System.out.println("MAIN THREAD STOPPED");
+        ThreadEx1 t = new ThreadEx1("thread ex1");
+        t.start();
+        ThreadEx1 t2 = new ThreadEx1("thread2 ex1");
+        t2.start();
+
+
+        t.join();
+        t2.join();
+
+        int n = 2, m = 5;
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.ENGLISH);
+
+//        n = scanner.nextInt();
+//        m = scanner.nextInt();
+//
+//
+//
+//        for (int i = 0; i < n; i++)
+//        {
+//            new ThreadEx4("Thread_" + i, m);
+//        }
+
+
+
+        
+
+
+
+
+
+        scanner.close();
     }
 }
